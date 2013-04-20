e88abef3968b73c9c852486834b231f4b422c11b
Andrew Clayton <andrew@digital-domain.net>
2013-04-20 15:00:56 +0100

    Update my Email address
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- cashflo-qp.c
+++ cashflo-qp.c
@@ -2,7 +2,7 @@
  * cashflo-qp.c - Calculate interest lost vs interest gaine
  *
  * Copyright (C) 2012-2013	OpenTech Labs
- * 				Andrew Clayton <andrew@opentechlabs.co.uk>
+ * 				Andrew Clayton <andrew@digital-domain.net>
  *
  * Released under the GNU General Public License version 2.
  * See COPYING
